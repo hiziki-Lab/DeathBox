78086704acc85ae34829d7fd0382676e95dc25e3
hi-kun-JPN <bbremon321@gmail.com>
2022-11-16 01:57:44 +0900

    コンソールからコマンドが実行された場合でもSendMessageでメッセージを送るように

--- src/main/java/xyz/hiziki/deathbox/command/Box.java
+++ src/main/java/xyz/hiziki/deathbox/command/Box.java
@@ -4,17 +4,13 @@ import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 import xyz.hiziki.deathbox.Main;
 import xyz.hiziki.deathbox.util.Prefix;
 
 public class Box implements CommandExecutor
 {
-    private final JavaPlugin plugin = Main.getPlugin();
-
     private final YamlConfiguration boxes = Main.getBoxes();
 
     @SuppressWarnings("NullableProblems")
@@ -23,14 +19,7 @@ public class Box implements CommandExecutor
     {
         if (!(sender instanceof Player p))
         {
-            if (sender instanceof ConsoleCommandSender)
-            {
-                plugin.getLogger().info("コマンドを実行できるのはプレイヤーのみです。");
-            }
-            else
-            {
-                sender.sendMessage("コマンドを実行できるのはプレイヤーのみです。");
-            }
+            sender.sendMessage("コマンドを実行できるのはプレイヤーのみです。");
         }
         else
         {
