2a488a718de5172fe29416a86b3ea6617bb8c267
hi-kun-JPN <bbremon321@gmail.com>
2022-11-17 10:50:57 +0900

    細かい修正

--- src/main/java/xyz/hiziki/deathbox/util/Prefix.kt
+++ src/main/java/xyz/hiziki/deathbox/util/Prefix.kt
@@ -8,7 +8,6 @@ class Prefix(p : Player, msg : String)
     init
     {
         p.sendMessage(ChatColor.AQUA.toString() + "[" + ChatColor.GREEN + "Boxプラグイン" + ChatColor.AQUA + "]"
-                + ChatColor.RESET + msg
-        )
+                + ChatColor.RESET + msg)
     }
 }
