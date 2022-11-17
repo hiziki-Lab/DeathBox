0a133364a68931d13af61bfd249c182b45fc4e97
hi-kun-JPN <bbremon321@gmail.com>
2022-11-17 10:42:44 +0900

    Mainクラスを最適化&プラグイン起動時にファイルを保存する必要がないので削除

--- src/main/java/xyz/hiziki/deathbox/Main.kt
+++ src/main/java/xyz/hiziki/deathbox/Main.kt
@@ -5,7 +5,6 @@ import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
 import xyz.hiziki.deathbox.command.Box
 import xyz.hiziki.deathbox.event.PlayerDeath
-import xyz.hiziki.deathbox.util.SaveFile
 import java.io.File
 
 class Main : JavaPlugin()
@@ -14,10 +13,13 @@ class Main : JavaPlugin()
     {
         // プラグイン起動時の処理
         super.onEnable()
+
         boxFile = File(dataFolder, "Boxes.yml")
         boxes = YamlConfiguration.loadConfiguration(boxFile!!)
+
         getCommand("box")!!.setExecutor(Box())
         Bukkit.getPluginManager().registerEvents(PlayerDeath(), this)
+
         logger.info("プラグインは正常に起動しました。")
     }
 
@@ -25,15 +27,14 @@ class Main : JavaPlugin()
     {
         // プラグイン停止時の処理
         super.onDisable()
-        SaveFile()
+
         logger.info("プラグインは正常に停止しました。")
     }
 
     companion object
     {
         var boxFile : File? = null
-            private set
+
         var boxes : YamlConfiguration? = null
-            private set
     }
 }
