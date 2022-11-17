5fe8b0a6b25188f0e44de6237ee3f23cf5658e95
hi-kun-JPN <bbremon321@gmail.com>
2022-11-17 10:47:18 +0900

    Boxクラスを最適化

--- src/main/java/xyz/hiziki/deathbox/command/Box.kt
+++ src/main/java/xyz/hiziki/deathbox/command/Box.kt
@@ -12,30 +12,32 @@ import xyz.hiziki.deathbox.util.Prefix
 
 class Box : CommandExecutor
 {
-    private val boxes : YamlConfiguration? = Main.Companion.getBoxes()
+    private val boxes : YamlConfiguration = Main.boxes!!
+
     override fun onCommand(sender : CommandSender, command : Command, label : String, args : Array<String>) : Boolean
     {
         if (sender !is Player)
         {
             sender.sendMessage("コマンドを実行できるのはプレイヤーのみです。")
-        } else
+        }
+        else
         {
-            val p = sender
-            if (boxes!!.getString("Boxes." + p.uniqueId + ".Location") == null)
+            if (boxes.getString("Boxes." + sender.uniqueId + ".Location") == null)
             {
-                Prefix(p, ChatColor.RED.toString() + "Boxデータが見つかりません")
-            } else
+                Prefix(sender, ChatColor.RED.toString() + "Boxデータが見つかりません")
+            }
+            else
             {
-                val loc = boxes.getLocation("Boxes." + p.uniqueId + ".Location")!!
+                val loc = boxes.getLocation("Boxes." + sender.uniqueId + ".Location")!!
+
                 if (loc.block.type == Material.CHEST)
                 {
-                    Prefix(
-                        p,
-                        ChatColor.AQUA.toString() + "あなたのBoxは" + loc.world!!.name + "の" + "X:" + loc.blockX + ", " + "Y:" + loc.blockY + ", " + "Z:" + loc.blockZ + "にあります。"
-                    )
-                } else
+                    Prefix(sender, ChatColor.AQUA.toString() + "あなたのBoxは" + loc.world!!.name + "の" +
+                            "X:" + loc.blockX + ", " + "Y:" + loc.blockY + ", " + "Z:" + loc.blockZ + "にあります。")
+                }
+                else
                 {
-                    Prefix(p, ChatColor.RED.toString() + "あなたのBoxは何者かによって破壊されています。")
+                    Prefix(sender, ChatColor.RED.toString() + "あなたのBoxは何者かによって破壊されています。")
                 }
             }
             return true
